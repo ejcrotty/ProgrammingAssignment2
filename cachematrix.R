9bc47f114d92f88e2f2adb2b1e533cf019ed4750
Ed Crotty <ejcrotty@gmail.com>
2016-09-30 23:34:22 -0500

    Initial
    fleshed out the 2 functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## ejcrotty@gmail.com
+##
+## inverting a matrix could take a while if it is large - so using a cache would eliminate
+## doing it more than once.  Which would save possibly a lot of time.
+##  we use the same structure as the example with the cached means of a vector
+##  except this is for a matrix and uses the solve function to invert
 
-## Write a short comment describing this function
+##The first function, makeCacheMatcrix  is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the inverse value of  the matric
+## get the inverse value of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invr <- NULL
+  set <- function(y) {
+      x <<- y
+      invr <<- NULL
+      }
+  get <- function() x
+  setinverse <- function(inverse) invr <<- inverse
+  getinverse <- function() invr
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve uses the solve function to invert the matrix
+## OR
+## if it has already been solved, it will return it from cache 
+## instead of doing it again ( solve could take a while if the matrix is big ) 
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invr <- x$getinverse()
+## if it already exists        
+  if(!is.null(invr)) {
+    message("getting cached data.")
+    ## return the already existing      
+    return(invr)
+  }
+  ## otherwise use solve to invert 
+  ## copy it into "data"      
+  data <- x$get()
+  ## invert "data" and store it in invr      
+  invr <- solve(data)
+  ## call the setinverse function to store it global      
+  x$setinverse(invr)
+  ## return it!      
+  invr
 }
